6cc76fb61c68e503340e1766ee9d18fd43a93d7a
sharooqalmashani1-afk <sharooqalmashani1@gmail.com>
2025-11-01 21:42:17 +0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('booking-form').addEventListener('submit',function(e){
- e.preventDefault();
- document.getElementById('success-message').classList.remove('hidden');
- setTimeout(()=>{document.getElementById('success-message').classList.add('hidden');},4000);
-});
